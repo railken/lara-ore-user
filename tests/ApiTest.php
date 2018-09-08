12328ca38bf2d41f19c5be0dfb84dac7e5d74e3f
railken <railken@protonmail.ch>
2018-09-08 19:53:51 +0200

    fix test

--- tests/ApiTest.php
+++ tests/ApiTest.php
@@ -3,12 +3,12 @@
 namespace Railken\LaraOre\User\Tests;
 
 use Illuminate\Support\Facades\Config;
-use Railken\LaraOre\Support\Testing\ApiTestableTrait;
+use Railken\LaraOre\Api\Support\Testing\TestableTrait;
 use Railken\LaraOre\User\UserFaker;
 
 class ApiTest extends BaseTest
 {
-    use ApiTestableTrait;
+    use TestableTrait;
 
     /**
      * Retrieve basic url.
@@ -17,7 +17,7 @@ class ApiTest extends BaseTest
      */
     public function getBaseUrl()
     {
-        return Config::get('ore.api.router.prefix').Config::get('ore.user.http.admin.router.prefix');
+        return Config::get('ore.api.http.admin.router.prefix').Config::get('ore.user.http.admin.router.prefix');
     }
 
     /**
