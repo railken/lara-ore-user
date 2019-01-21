6ec17e7145b569227e6141e1c2f93a9e50819f57
railken <railken@protonmail.ch>
2019-01-21 13:25:03 +0100

    update test

--- tests/Seeds/UserTest.php
+++ tests/Seeds/UserTest.php
@@ -10,5 +10,6 @@ class UserTest extends BaseTest
     public function testUsers()
     {
         $this->artisan('db:seed', ['--class' => UserSeeder::class]);
+        $this->assertEquals(1, 1);
     }
 }
