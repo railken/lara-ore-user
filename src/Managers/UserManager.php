7c4139de589d52191204df46a69864f541de8aba
railken <railken@protonmail.ch>
2019-07-14 13:05:00 +0200

    phpdoc

--- src/Managers/UserManager.php
+++ src/Managers/UserManager.php
@@ -5,6 +5,14 @@ namespace Amethyst\Managers;
 use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
+/**
+ * @method \Amethyst\Models\User newEntity()
+ * @method \Amethyst\Schemas\UserSchema getSchema()
+ * @method \Amethyst\Repositories\UserRepository getRepository()
+ * @method \Amethyst\Serializers\UserSerializer getSerializer()
+ * @method \Amethyst\Validators\UserValidator getValidator()
+ * @method \Amethyst\Authorizers\UserAuthorizer getAuthorizer()
+ */
 class UserManager extends Manager
 {
     use ConfigurableManager;
